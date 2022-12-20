b77ce8056143ed4f477bf762b2973cd66880236a
Ndekocode <arickbulakali@gmail.com>
2022-12-20 21:20:10 +0200

    add block logo html

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,12 @@ if (!varIsEmpty(modalCloseOverlay) && !varIsEmpty(modalCloseBtn)) {
 function varIsEmpty(v) {
   return v === undefined || v === null || v.length < 1;
 }
+
+// Notification TOAST variable
+const notificationToast = document.querySelector("[data-toast]");
+const toastCloseBtn = document.querySelector("[data-toast-close]");
+if (!varIsEmpty(toastCloseBtn)) {
+  toastCloseBtn.addEventListener("click", function () {
+    notificationToast.classList.add("closed");
+  });
+}
